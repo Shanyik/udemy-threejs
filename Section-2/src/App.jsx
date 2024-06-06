f0b7767b8bb0fef662e0ae777215336597f3351a
Shanyik <tkrisi213@gmail.com>
2024-06-06 16:10:13 +0200

    Section 2 - part 3 done

--- Section-2/src/App.jsx
+++ Section-2/src/App.jsx
@@ -2,29 +2,42 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Link
+  Link,
 } from 'react-router-dom';
 import './App.css';
 import Basics from './pages/Basics';
+import Loader from './pages/components/Loader';
 
 function App() {
-
   return (
     <Router>
       <div>
-        <nav style={{position: "fixed", top: 0, left: 0, zIndex: 1}}>
+        <nav
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            zIndex: 1,
+          }}
+        >
           <ul>
             <p>
               <Link to="/basics">part 1</Link>
+            </p>
+          </ul>
+          <ul>
+            <p>
+              <Link to="/textureLoader">part 2</Link>
             </p>
           </ul>
         </nav>
       </div>
       <Routes>
         <Route path="/basics" element={<Basics />} />
+        <Route path="/textureLoader" element={<Loader />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
